9748fd13929f3522db3e2d2a0f4e4c001ba67bea
VidadiMurad <vidad0murad@gmail.com>
2026-05-08 22:15:35 +0400

    First Task Finish

--- Orient.15.Tasks/Orient.15.Tasks/main.swift
+++ Orient.15.Tasks/Orient.15.Tasks/main.swift
@@ -5,7 +5,19 @@
 //  Created by Vidadi Muradzada on 08.05.26.
 //  First Task
 
+
+// Library
 import Foundation
 
-print("Hello, World!")
+// Variable
+var sumOfNums: Int = 0
 
+// Loop
+for i in 1...50{
+  sumOfNums = sumOfNums + i
+  continue
+}
+
+print("---------------------------------")
+print("The Answer is: \(sumOfNums)")
+print("---------------------------------")
