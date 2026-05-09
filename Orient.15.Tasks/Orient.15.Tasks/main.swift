441422121c22f7e7302c24183e053a5fd6f10423
VidadiMurad <vidad0murad@gmail.com>
2026-05-09 15:17:00 +0400

    Task 6 Finish

--- Orient.15.Tasks/Orient.15.Tasks/main.swift
+++ Orient.15.Tasks/Orient.15.Tasks/main.swift
@@ -89,3 +89,66 @@ if let realScore = score {
 }
 */
 //-----------------------------------------------
+
+
+
+
+
+//                                         6 TASK
+
+/*
+import Foundation
+var isUser = false
+var isPass = false
+
+func checkLogin(username: String?, password: String?){
+    if let _ = username {
+        isUser = true
+    }
+    if let _ = password {
+       isPass = true
+    }
+    
+    if isUser && isPass {
+        print("Giriş Uğurludur!")
+    }else{
+        print("Giriş Natamamdır!")
+    }
+}
+
+checkLogin(username: "Vidadi", password: "123456")
+*/
+
+/*
+import Foundation
+
+var isUser = false
+var isPass = false
+
+func checkLogin(username: String?, password: String?) {
+    guard let _ = username else {
+       return
+    }
+    isUser = true
+    
+    guard let _ = password else {
+        return
+    }
+    isPass = true
+    
+    
+    
+    guard isUser && isPass else {
+        print("GiriŞ")
+        return
+    }
+    print("Giriş Uğurludur!")
+}
+
+
+checkLogin(username: nil, password: "123456")
+*/
+
+
+
+
